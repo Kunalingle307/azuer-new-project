28f8c679e3d37855859694e15f77f2b29fd412b9
Kunalingle307 <130652340+Kunalingle307@users.noreply.github.com>
2024-02-26 13:33:43 +0530

    Update db

--- database/db.php
+++ database/db.php
@@ -1,5 +1,5 @@
 <?php
-	$database_username = 'severonline';//user name
-	$database_password = 'Redbull9';//password
-	$pdo_conn = new PDO( 'mysql:host=localhost;dbname=blog_admin_db', $database_username, $database_password );
+	$database_username = 'onlineblog';//user name
+	$database_password = '@Kunal';//password
+	$pdo_conn = new PDO( 'mysql:host=onlineblog.mysql.database.azure.com;dbname=blog_admin_db', $database_username, $database_password );
 ?>
