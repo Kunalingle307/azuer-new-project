b0ea542b9b1c7426d35bd1717822e63a0cd948a4
Kunalingle307 <130652340+Kunalingle307@users.noreply.github.com>
2024-02-26 14:54:03 +0530

    update

--- database/conn.php
+++ database/conn.php
@@ -1,4 +1,4 @@
 <?php
 //database connection
-($GLOBALS["___mysqli_ston"] = new mysqli("onlineblog.mysql.database.azure.com","amit","kunal@16","blog_admin_db"));  //host,user,password,database
+($GLOBALS["___mysqli_ston"] = new mysqli("onlineblog.mysql.database.azure.com","onlineblog","kunal@16","blog_admin_db"));  //host,user,password,database
 ?>
